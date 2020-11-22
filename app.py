c3e6abaf751e57f0d2c9b8937f4b3c540ebf0423
ashwi <ashleywntr@gmail.com>
2020-11-22 23:32:53 +0000

    CSV Export functionality added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import abort
 from flask import request, jsonify
 from datetime import datetime, timedelta
 from flask_cors import CORS
+from flask_csv import send_csv
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 import json
@@ -22,6 +23,79 @@ def hello_world():
 @app.route('/api/welcomestats', methods=['GET'])
 def api_welcome_stats():
     pass
+
+
+@app.route('/api/csv/block')
+def api_csv_block_list():
+    date_string = request.args['date']
+    block_data_for_csv = []
+
+    date_object_from = datetime.strptime(date_string, '%d%m%Y')
+    return_blockday = data_structures.BlockDay(date_object_from)
+    data_retrieval = return_blockday.data_retrieval(outline_only_request=True)
+    block_list = data_retrieval['blocks']
+
+    for x in block_list:
+        result = data_structures.block_collection.find_one(x)
+        result['hash'] = result.pop('_id')
+        block_data_for_csv.append(result)
+
+    block_data_for_csv.sort(key=lambda item: item.get("height"))
+    assert len(block_list) == len(block_data_for_csv)
+
+    fields_list = ["height", "average_fee_per_transaction", "average_num_inputs_per_transaction",
+                   "average_num_outputs_per_transaction", "average_val_inputs_per_transaction",
+                   "average_val_outputs_per_transaction",
+                   "fee",
+                   "main_chain",
+                   "mrkl_root",
+                   "n_tx",
+                   "nonce",
+                   "prev_block",
+                   "size",
+                   "time",
+                   "total_num_inputs_block",
+                   "total_num_outputs_block",
+                   "total_val_fees_block",
+                   "total_val_inputs_block",
+                   "total_val_outputs_block", "hash"]
+
+    return send_csv(block_data_for_csv, filename=f"Blocks {date_string}.csv", fields=fields_list,
+                    writer_kwargs={"extrasaction": "ignore"})
+
+
+@app.route('/api/csv/transactions')
+def api_csv_transaction_list():
+    search_hash = request.args['hash']
+    transaction_data_for_csv = []
+
+    block_result = data_structures.block_collection.find_one(search_hash)
+
+    for tx in block_result['tx']:
+        result = data_structures.transaction_collection.find_one(tx)
+
+        result["number_of_inputs"] = result.pop('vin_sz')
+        result["number_of_outputs"] = result.pop('vout_sz')
+        result["transaction_hash"] = result.pop('_id')
+
+        transaction_data_for_csv.append(result)
+
+    transaction_data_for_csv.sort(key=lambda item: item.get("time"))
+    assert len(block_result['tx']) == len(transaction_data_for_csv)
+
+    fields_list = ["transaction_hash",
+                   "block_height",
+                   "number_of_inputs",
+                   "number_of_outputs",
+                   "coinbase_transaction",
+                   "time",
+                   "size",
+                   "value_inputs",
+                   "fee",
+                   "value_outputs"]
+
+    return send_csv(transaction_data_for_csv, filename=f"Transactions for Block {block_result['height']}.csv",
+                    fields=fields_list, writer_kwargs={"extrasaction": "ignore"})
 
 
 @app.route('/api/blockdays', methods=['GET'])
