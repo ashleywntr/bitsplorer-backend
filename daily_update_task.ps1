1224c51ddd9019ecbe81d9bae064b5745d0b9603
ashwi <ashleywntr@gmail.com>
2021-04-09 00:18:26 +0100

    correct local ip for script request

--- daily_update_task.ps1
+++ daily_update_task.ps1
@@ -1,4 +1,4 @@
 $date = (Get-Date).AddDays(-1)
 $format_date = $date.ToString("yyyy-MM-dd")
-(New-Object Net.WebClient).DownloadString("http://192.168.1.194:55397/api/blockdays?date=$format_date")
+(New-Object Net.WebClient).DownloadString("http://192.168.0.18:55397/api/blockdays?date=$format_date")
 
