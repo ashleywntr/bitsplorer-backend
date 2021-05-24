100957ac95e9f091a9808d9fce57ebe856d432bd
ashwi <ashleywntr@gmail.com>
2021-05-24 15:51:50 +0100

    landing page, separation of currency logic

---
+++ bitsplorer-api/currency_logic.py
@@ -0,0 +1,72 @@
+from datetime import datetime, timedelta, timezone
+
+import requests
+
+import data_structures
+import flask_app
+
+
+def currency_data_retriever(retrieval_date_from, retrieval_date_to):
+    coindesk_url = f'https://api.coindesk.com/v1/bpi/historical/close.json?start={retrieval_date_from}&end={retrieval_date_to}'
+    print('Retrieving Currency Information from', coindesk_url)
+    currency_request = requests.get(coindesk_url, headers=data_structures.default_headers)
+    currency_request.raise_for_status()
+    currency_data = currency_request.json()['bpi'].items()
+
+    exchange_rate_data = exchange_data_retrieval(retrieval_date_from, retrieval_date_to)
+    consolidated_data = {}
+    for working_date, usd_value in currency_data:
+        consolidated_data[working_date] = {
+            'USD': usd_value,
+        }
+        exchange_date = working_date
+        while exchange_date not in exchange_rate_data:
+            exchange_date_object = datetime.fromisoformat(exchange_date).replace(tzinfo=timezone.utc)
+            subtracted_date = (exchange_date_object - timedelta(days=1))
+            exchange_date = subtracted_date.strftime('%Y-%m-%d')
+
+        for currency in flask_app.currency_list:
+            if currency in exchange_rate_data[exchange_date]:
+                consolidated_data[working_date][currency] = round(
+                    float(usd_value) * float(exchange_rate_data[exchange_date][currency]), 2)
+    return consolidated_data
+
+
+def exchange_data_retrieval(date_from, date_to):
+    date_object_from = datetime.fromisoformat(date_from)
+    date_object_to = datetime.fromisoformat(date_to)
+
+    date_object_from.replace(tzinfo=timezone.utc)
+    date_object_to.replace(tzinfo=timezone.utc)
+
+    from_weekday = date_object_from.weekday()
+
+    if from_weekday > 4:
+        date_object_from = date_object_from - timedelta(days=from_weekday - 4)
+        date_from = date_object_from.strftime('%Y-%m-%d')
+        print('Exchange retrieval updated to proceeding Friday')
+    try:
+        exchange_base_url = "https://freecurrencyapi.net/api/v1/rates"
+        exchange_api_key = "72741890-bc06-11eb-81e3-cd95bf08b9a9"
+        exchange_rate_retrieval_url = f"{exchange_base_url}?base_currency=USD&date_from={date_from}&date_to={date_to}&apikey={exchange_api_key}"
+
+        print(f"Retrieving Exchange Rate Data from {exchange_rate_retrieval_url}")
+        exchange_headers = data_structures.default_headers
+        exchange_headers['apikey'] = exchange_api_key
+        exchange_request = requests.get(url=exchange_rate_retrieval_url, headers=exchange_headers, timeout=15)
+        exchange_request.raise_for_status()
+        exchange_rate_data = exchange_request.json()['data']
+
+    except requests.exceptions.HTTPError as ex:
+        if ex.response.status_code == 500:
+            date_object_from = date_object_from - timedelta(days=1)
+            date_from = date_object_from.strftime('%Y-%m-%d')
+            return exchange_data_retrieval(date_from, date_to)
+        if not ex.response.status_code == 429:
+            print('Other HTTP error occurred', ex)
+    except requests.exceptions.ReadTimeout as timeout:
+        print("Request read timeout", timeout)
+    except requests.exceptions.ConnectionError as connection_error:
+        print("Connection Error Occurred", connection_error)
+    else:
+        return exchange_rate_data
--- bitsplorer-api/flask_app.py
+++ bitsplorer-api/flask_app.py
@@ -1,8 +1,7 @@
 import gc
 import json
 import traceback
-from concurrent.futures import as_completed
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 
 import requests
 from flask import Flask
@@ -11,8 +10,8 @@ from flask import request
 from flask_cors import CORS
 from flask_csv import send_csv
 from flask_executor import Executor
-from requests_futures.sessions import FuturesSession
 
+import currency_logic
 import data_structures
 from data_structures import BlockDay, Address
 from project_enum import RetrievalType
@@ -27,18 +26,24 @@ currency_list = ['GBP', 'EUR', 'CNY']
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    page_content = ""
+    page_content += "<div><h1> Bitsplorer Backend </h1></div>"
+    page_content += "\n<div><h3> Please choose some example functions</h3>"
 
+    page_content += "\n<h4> Block Explorer Examples </h4>"
+    page_content += '\n<a href="../api/blockdays?date=2020-07-31"> BlockDay Request </a><br>'
+    page_content += '\n<a href="../api/csv/block?date=2020-07-31"> BlockDay CSV Request </a><br>'
+    page_content += '\n<a href="../api/block?hash=000000000000000000042be75dce3cb116c9720801a9208bcb556c6f8604b059"> Block Request </a><br>'
+    page_content += '\n<a href="../api/transaction?hash=f205316b3c722247c6bad75e71a9e2e14d1f7329c042ff7797593cd67f982686"> Transaction Request </a>'
 
-@app.route('/api', methods=['GET'])
-def hello_api_world():
-    return 'Hello api World!'
+    page_content += "\n<h4> Address Explorer Examples </h4>"
+    page_content += '\n<a href="../api/address?hash=172fgNkRaoDmAqpbkct5TUYBr1PodCF4pZ"> Address Request </a>'
 
-
-@app.route('/api/welcomestats', methods=['GET'])
-def api_welcome_stats():
-    pass
-
+    page_content += "\n<h4> Currency Examples </h4>"
+    page_content += '\n<a href="../api/currency?date_from=2020-07-19&date_to=2020-07-31"> Currency JSON Request </a><br>'
+    page_content += '\n<a href="../api/csv/currency?date_from=2020-05-07&date_to=2020-05-11"> Currency CSV Request </a>'
+    page_content += ("\n</div>")
+    return page_content
 
 @app.route('/api/visualisation/sunburst', methods=['GET'])
 def api_sunburst_visualisation():
@@ -124,7 +129,7 @@ def api_csv_currency_report():
 
     retrieval_date_from = request.args['date_from']
     retrieval_date_to = request.args['date_to']
-    currency_json_data = currency_data_retriever(retrieval_date_from, retrieval_date_to)
+    currency_json_data = currency_logic.currency_data_retriever(retrieval_date_from, retrieval_date_to)
 
     currency_csv_data = [dict(value, date=key) for key, value in currency_json_data.items()]
     return send_csv(currency_csv_data, filename=f"Currency Data {str(retrieval_date_from), str(retrieval_date_to)}.csv",
@@ -254,72 +259,7 @@ def api_address_transactions():
 def api_currency_date():
     retrieval_date_from = request.args['date_from']
     retrieval_date_to = request.args['date_to']
-    return currency_data_retriever(retrieval_date_from, retrieval_date_to)
-
-
-def currency_data_retriever(retrieval_date_from, retrieval_date_to):
-    coindesk_url = f'https://api.coindesk.com/v1/bpi/historical/close.json?start={retrieval_date_from}&end={retrieval_date_to}'
-    print('Retrieving Currency Information from', coindesk_url)
-    currency_request = requests.get(coindesk_url, headers=data_structures.default_headers)
-    currency_request.raise_for_status()
-    currency_data = currency_request.json()['bpi'].items()
-
-    exchange_rate_data = exchange_data_retrieval(retrieval_date_from, retrieval_date_to)
-    consolidated_data = {}
-    for working_date, usd_value in currency_data:
-        consolidated_data[working_date] = {
-            'USD': usd_value,
-        }
-        exchange_date = working_date
-        while exchange_date not in exchange_rate_data:
-            exchange_date_object = datetime.fromisoformat(exchange_date).replace(tzinfo=timezone.utc)
-            subtracted_date = (exchange_date_object - timedelta(days=1))
-            exchange_date = subtracted_date.strftime('%Y-%m-%d')
-
-        for currency in currency_list:
-            if currency in exchange_rate_data[exchange_date]:
-                consolidated_data[working_date][currency] = round(float(usd_value) * float(exchange_rate_data[exchange_date][currency]), 2)
-    return consolidated_data
-
-
-def exchange_data_retrieval(date_from, date_to):
-    date_object_from = datetime.fromisoformat(date_from)
-    date_object_to = datetime.fromisoformat(date_to)
-
-    date_object_from.replace(tzinfo=timezone.utc)
-    date_object_to.replace(tzinfo=timezone.utc)
-
-    from_weekday = date_object_from.weekday()
-
-    if from_weekday > 4:
-        date_object_from = date_object_from - timedelta(days=from_weekday-4)
-        date_from = date_object_from.strftime('%Y-%m-%d')
-        print('Exchange retrieval updated to proceeding Friday')
-    try:
-        exchange_base_url = "https://freecurrencyapi.net/api/v1/rates"
-        exchange_api_key="72741890-bc06-11eb-81e3-cd95bf08b9a9"
-        exchange_rate_retrieval_url = f"{exchange_base_url}?base_currency=USD&date_from={date_from}&date_to={date_to}&apikey={exchange_api_key}"
-
-        print(f"Retrieving Exchange Rate Data from {exchange_rate_retrieval_url}")
-        exchange_headers = data_structures.default_headers
-        exchange_headers['apikey'] = exchange_api_key
-        exchange_request = requests.get(url=exchange_rate_retrieval_url, headers=exchange_headers, timeout=15)
-        exchange_request.raise_for_status()
-        exchange_rate_data = exchange_request.json()['data']
-
-    except requests.exceptions.HTTPError as ex:
-        if ex.response.status_code == 500:
-            date_object_from = date_object_from - timedelta(days=1)
-            date_from = date_object_from.strftime('%Y-%m-%d')
-            return exchange_data_retrieval(date_from, date_to)
-        if not ex.response.status_code == 429:
-            print('Other HTTP error occurred', ex)
-    except requests.exceptions.ReadTimeout as timeout:
-        print("Request read timeout", timeout)
-    except requests.exceptions.ConnectionError as connection_error:
-        print("Connection Error Occurred", connection_error)
-    else:
-        return exchange_rate_data
+    return currency_logic.currency_data_retriever(retrieval_date_from, retrieval_date_to)
 
 
 if __name__ == '__main__':
